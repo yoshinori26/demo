0b903bb663f10d62bf0c289eddf735ebd4fd2cdb
EC2 Default User <ec2-user@ip-172-31-44-170.us-east-2.compute.internal>
2018-04-30 03:33:49 +0000

    demo

---
+++ app/controllers/lessons_controller.rb
@@ -0,0 +1,7 @@
+class LessonsController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'lessons/home'
+
+  get 'lessons/about'
+
+  get 'lessons/help'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
